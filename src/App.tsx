c6b2bfd4b4fcd811f03c3ab6832c909c4fa35ae1
Octrainn <Akshay2334@hotmail.com>
2023-04-01 00:38:49 -0400

    starting commit

---
+++ src/App.tsx
@@ -0,0 +1,33 @@
+import "./styles.css";
+
+function App() {
+  return (
+    <div className="calculator-flex">
+      <div className="output">
+        <div className="top-right-number">
+          <div className="main-number"></div>
+        </div>
+      </div>
+      <button className="two-spots">AC</button>
+      <button>DEL</button>
+      <button>÷</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>*</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>+</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>-</button>
+      <button>.</button>
+      <button>0</button>
+      <button className="two-spots">=</button>
+    </div>
+  );
+}
+
+export default App;
